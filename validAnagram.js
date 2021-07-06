56977eadbf3086a477cd37524c9d34a80d0b4f29
Michael Sutton <hicallmesutton@gmail.com>
2021-07-06 16:40:20 -0500

    update to validAnagram

--- validAnagram.js
+++ validAnagram.js
@@ -1,13 +1,14 @@
 function validAnagram(str1, str2){
   // add whatever parameters you deem necessary - good luck!
   if (str1.length !== str2.length ) {
-      return false;
+      return false
   }
 
   let strObj = {};
 
   for (let val in str1 ) {
-      //console.log(str1[val])
+      // for strings you need to do strObj[str1[val]]
+
       // if key exists add to it if not create it
       strObj[str1[val]] = (strObj[str1[val]] || 0) + 1;
   }
@@ -18,14 +19,11 @@ function validAnagram(str1, str2){
 
   // now loop over the to see if the values are even
   for (let key in strObj) {
-      console.log(strObj)
-      console.log(strObj[key])
-
       if (strObj[key] % 2 !== 0) {
           return false
       }
   }
 
+  return true
 
-  return true;
 }
