fdddd8c9f98051970f31f293e36a02155604aa2e
Michael Sutton <hicallmesutton@gmail.com>
2021-07-06 23:11:01 -0500

    divideAndConquer-Intro

---
+++ divideAndConquer-Intro.js
@@ -0,0 +1,26 @@
+// This name can actually be googled
+// search() is actually binarySearch
+// must be sorted
+// Time Complexity - Log(n)
+function search(array, val) {
+
+  let min = 0
+  let max = array.length - 1
+
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2)
+    let currentElement = array[middle]
+
+    if (array[middle] < val) {
+      min = middle + 1
+    }
+    else if (array[middle] > val) {
+      max = middle - 1
+    }
+    else {
+      return middle
+    }
+  }
+
+  return -1
+}
