f6cf6f738e6a46e943822a9ca06ee12ad7854098
Michael Sutton <hicallmesutton@gmail.com>
2021-07-12 17:35:42 -0500

    selectionSort

---
+++ selectionSort.js
@@ -0,0 +1,23 @@
+function selectionSort(arr) {
+  for (let i=0; i<arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+          min = j;
+
+      }
+    }
+    if (i !== min) {
+        console.log(arr)
+        console.log('swapping to: ')
+        let temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+        console.log(arr)
+        console.log('*********')
+    }
+  }
+  return arr;
+}
+
+selectionSort([0,2,34,22,10,19,17])
