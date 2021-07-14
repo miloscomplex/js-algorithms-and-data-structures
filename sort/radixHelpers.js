9ed2f165f9a31a79fc26e707ec074063b285a52b
Michael Sutton <hicallmesutton@gmail.com>
2021-07-14 00:15:17 -0500

    radixHelpers

---
+++ sort/radixHelpers.js
@@ -0,0 +1,19 @@
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.power(10, i)) % 10;
+
+}
+
+getDigit(7323,2)
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (var i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
