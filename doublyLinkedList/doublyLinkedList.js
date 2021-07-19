145a901dceee23b42e153e6b2f1602cdc8f5565f
Michael Sutton <hicallmesutton@gmail.com>
2021-07-19 00:34:49 -0500

    Doubly linkedList Node Class and DoublyLinkedList Class and push()

---
+++ doublyLinkedList/doublyLinkedList.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+  }
+
+  push(val) {
+    let node = new Node(val);
+    if (this.length === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail
+      this.tail = node;
+    }
+      this.length++;
+      return this;
+    }
+  }
+
+
+}
