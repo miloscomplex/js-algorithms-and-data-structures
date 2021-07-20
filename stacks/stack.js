5828e2051d563b681dda0d314a20e7ffcd75e2df
Michael Sutton <hicallmesutton@gmail.com>
2021-07-20 00:35:32 -0500

    created a stack class

---
+++ stacks/stack.js
@@ -0,0 +1,39 @@
+// LIFO last in first out
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    let node = new Node(val);
+    if (this.size === 0) {
+      this.first = node;
+      this.last = node;
+    } else {
+      let temp = this.first;
+      this.first = node;
+      this.first.next = temp;
+    }
+
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
+  }
+}
