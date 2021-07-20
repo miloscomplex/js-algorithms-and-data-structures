af54ef61bd4ba6b015f7ee7d2dea25328870f75f
Michael Sutton <hicallmesutton@gmail.com>
2021-07-20 18:08:21 -0500

    created Queue Class

--- sort/radixSort-algo.js
+++ sort/radixSort-algo.js
@@ -1,4 +1,4 @@
-function getDigit(num, i) {
+function getNum(num, i) {
   return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
 
@@ -7,7 +7,7 @@ function digitCount(num) {
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
 
-function mostDigits(nums) {
+function findMaxDigits(nums) {
   let maxDigits = 0;
   for (let i = 0; i < nums.length; i++) {
     maxDigits = Math.max(maxDigits, digitCount(nums[i]));
@@ -15,12 +15,12 @@ function mostDigits(nums) {
   return maxDigits;
 }
 
-function radixSort(nums){
-    let maxDigitCount = mostDigits(nums);
+function radixSort(nums) {
+    let maxDigitCount = findMaxDigits(nums);
     for(let k = 0; k < maxDigitCount; k++){
         let digitBuckets = Array.from({length: 10}, () => []);
         for(let i = 0; i < nums.length; i++){
-            let digit = getDigit(nums[i],k);
+            let digit = getNum(nums[i],k);
             digitBuckets[digit].push(nums[i]);
         }
         nums = [].concat(...digitBuckets);
