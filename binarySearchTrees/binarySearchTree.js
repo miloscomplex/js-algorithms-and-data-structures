343dd21f8ecd55350e01698cf1f75a5a751e7d4a
Michael Sutton <hicallmesutton@gmail.com>
2021-07-20 19:04:26 -0500

    created binarySearchTree with insert()

---
+++ binarySearchTrees/binarySearchTree.js
@@ -0,0 +1,69 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    let newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while(true) {
+        if(value === current.value) return undefined;
+        if (val < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  insert(val) {
+    let newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    while(true) {
+      if (value === current.value) return undefined;
+      if (val < current.value) {
+        if (current.left === null) {
+            current.left = newNode;
+            return this;
+          }
+          current = current.left;
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          }
+          current = current.right;
+          }
+      }
+  }
+
+}
