f29659375d8cf957a3e1967ae5ef68a672ee09bb
Michael Sutton <hicallmesutton@gmail.com>
2021-07-27 01:04:38 -0500

    added removeEdge() & removeVertex() to Graph Class

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -12,8 +12,26 @@ class Graph {
   }
 
   addEdge(vertex1, vertex2) {
+    // can add verfication statements
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertext2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      v => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      v => v !== vertex1
+    );
+  }
+
+  removeVertex(vertex) {
+    while(this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertext].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex]
   }
 
 }
