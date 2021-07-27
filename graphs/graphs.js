c2b240d85ef431ee302a039156b03c4c4e71d4b3
Michael Sutton <hicallmesutton@gmail.com>
2021-07-27 00:41:45 -0500

    Created Graph Class and added addVertex()

---
+++ graphs/graphs.js
@@ -0,0 +1,14 @@
+// you could do a matrix, but if your edges are sparse matrix will be slower
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+  
+}
