351d3b13a89fd6244a35a18c06470923f50f7a7b
Michael Sutton <hicallmesutton@gmail.com>
2021-07-28 01:06:10 -0500

    added depthFirstRecursive() to Graph Class

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -34,4 +34,50 @@ class Graph {
     delete this.adjacencyList[vertex]
   }
 
+  depthFirstRecursive(start) {
+    const results = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function traverse(vertex) {
+        if (!vertex) return null;
+        visited[vertex] = true;
+        results.push(vertex);
+        adjacencyList[vertex].forEach(neighbor => {
+          if (!visited[neighbor]) {
+            return traverse(neighbor)
+          }
+        })
+    })(start);
+
+    return results;
+  }
+
 }
+
+let g = new Graph();
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B","D")
+g.addEdge("C","E")
+g.addEdge("D","E")
+g.addEdge("D","F")
+g.addEdge("E","F")
+g.depthFirstRecursive("A")
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
