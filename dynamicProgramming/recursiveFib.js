c8ffd666b4258d665d9b076b01d5bee58687bb73
Michael Sutton <hicallmesutton@gmail.com>
2021-08-09 17:06:22 -0500

    Added tabulated for Fib Algo

--- dynamicProgramming/recursiveFib.js
+++ dynamicProgramming/recursiveFib.js
@@ -2,3 +2,13 @@ function fib(n) {
   if (n <= 2) return 1;
   return fib(n - 1) + fib(n - 2);
 }
+
+// memoized version
+function fib(n, memo=[]) {
+  if (memo[n] !== undefined) return memo[n];
+  // if this n exists in memo we are done
+  if (n <= 2) return 1;
+  let res = fib(n-1, memo) + fib(n-2, memo);
+  memo[n] = res;
+  return res;
+}
